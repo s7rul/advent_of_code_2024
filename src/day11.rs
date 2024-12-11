90bc64e85c3b2625ee0cff32636968671c87e51a
Erik Serrander <erik.serrander@grepit.se>
2024-12-11 06:38:50 +0100

    day 11 part 1

---
+++ src/day11.rs
@@ -0,0 +1,73 @@
+#![allow(clippy::comparison_chain)]
+use std::usize;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day11)]
+pub fn generator(input: &str) -> Vec<u64> {
+    input.split_whitespace().map(|v| v.parse().unwrap()).collect()
+}
+
+fn count_digits(v: u64) -> u64 {
+    let mut n: u64 = 0;
+    while v / 10_u64.pow(n as u32) > 0 {
+        n += 1;
+    }
+    n
+}
+
+#[aoc(day11, part1)]
+pub fn solve_part1(input: &[u64]) -> usize {
+    let mut stones = input.to_vec();
+    for _ in 0..25 {
+        let mut i = 0;
+        while i < stones.len() {
+            let stone = stones[i];
+            if stone == 0 {
+                stones[i] = 1;
+                i += 1;
+            } else {
+                let digits = count_digits(stone);
+                if digits % 2 == 0 {
+                    let left = stone / 10_u64.pow(digits as u32 / 2);
+                    let right = stone % 10_u64.pow(digits as u32 / 2);
+                    stones[i] = right;
+                    stones.insert(i, left);
+                    i += 2;
+                } else {
+                    stones[i] = stone * 2024;
+                    i += 1;
+                }
+            }
+        }
+    }
+    stones.len()
+}
+
+#[aoc(day11, part2)]
+pub fn solve_part2(input: &[u64]) -> usize {
+    let mut stones = input.to_vec();
+    for blink in 0..75 {
+        let mut i = 0;
+        while i < stones.len() {
+            let stone = stones[i];
+            if stone == 0 {
+                stones[i] = 1;
+                i += 1;
+            } else {
+                let digits = count_digits(stone);
+                if digits % 2 == 0 {
+                    let left = stone / 10_u64.pow(digits as u32 / 2);
+                    let right = stone % 10_u64.pow(digits as u32 / 2);
+                    stones[i] = right;
+                    stones.insert(i, left);
+                    i += 2;
+                } else {
+                    stones[i] = stone * 2024;
+                    i += 1;
+                }
+            }
+        }
+    }
+    stones.len()
+}
